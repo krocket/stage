de8da69f677aa2650550aa433402ea053fd33e06
Amexandre Baron <aba@olabs.be>
2021-01-21 20:52:10 +0100

    Initial Commit

---
+++ plant_nursery/__manifest__.py
@@ -0,0 +1,17 @@
+# "- * - coding: utf - 8 -
+
+{
+    'name': 'Plant Nursery',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Plants and Customers Management',
+    'depends': ['web'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
