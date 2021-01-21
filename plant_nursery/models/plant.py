a24cf6c5d8ff5344dc222c97de9201f4e8fb4f6b
Amexandre Baron <aba@olabs.be>
2021-01-21 21:38:35 +0100

    Introduction des modèles basics

--- plant_nursery/models/plant.py
+++ plant_nursery/models/plant.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 
 
 class Plants(models.Model):
-    _name = 'nursery plant'
+    _name = 'nursery.plant'
     _description = 'Nursery Plant'
 
     name = fields.Char("Plant Name")
