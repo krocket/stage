d35ca4e46bd983f04ac943b978c90e8f9504d2b8
Amexandre Baron <aba@olabs.be>
2021-01-25 11:09:18 +0100

    plant_nursery: Ajout d'un champ de calcul sur les plantes

--- plant_nursery/models/plant.py
+++ plant_nursery/models/plant.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Plants(models.Model):
@@ -10,3 +10,11 @@ class Plants(models.Model):
     name = fields.Char("Plant Name")
     price = fields.Float()
     order_ids = fields.One2many("nursery.order", "plant_id", strings="Orders")
+    order_count = fields.Integer(compute='_compute_order_count',
+                                 store=True,
+                                 string="Total sold")
+
+    @api.depends('order_ids')
+    def _compute_order_count(self):
+        for plant in self:
+            plant.order_count = len(plant.order_ids)
