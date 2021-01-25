5c4b22283702792c0df5fe9dd6ac0183d6909ab1
Amexandre Baron <aba@olabs.be>
2021-01-25 10:49:31 +0100

    plant_nursery: Ajout d'un champ de calcul sur les plantes

--- plant_nursery/models/order.py
+++ plant_nursery/models/order.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models
+from odoo.exceptions import UserError
 
 
 class Order(models.Model):
     _name = 'nursery.order'
     _description = 'Nursery Order'
 
+    name = fields.Datetime(default=fields.Datetime.now)
     plant_id = fields.Many2one("nursery.plant", required=True)
     customer_id = fields.Many2one("nursery.customer")
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirm', 'Confirmed'),
+        ('cancel', 'Cancelled')
+    ], default='draft')
+    last_modification = fields.Datetime(readonly=True)
+    
+    def write(self, values):
+        values['last_modification'] = fields.Datetime.now()
+        
+        return super(Order, self).write(values)
+    
+    def unlink(self):
+        for order in self:
+            if order.state == 'confirm':
+                raise UserError("You can not delete confirmed orders")
+            
+        return super(Order, self).unlink()
