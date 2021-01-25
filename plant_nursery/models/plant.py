ca0ddad0272b796c8a38887d48ba4382d74696b1
Amexandre Baron <aba@olabs.be>
2021-01-25 11:27:57 +0100

    plant_nursery: Ajout d'un kanban view pour les plantes

--- plant_nursery/models/plant.py
+++ plant_nursery/models/plant.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 
 class Plants(models.Model):
@@ -13,8 +14,18 @@ class Plants(models.Model):
     order_count = fields.Integer(compute='_compute_order_count',
                                  store=True,
                                  string="Total sold")
+    number_in_stock = fields.Integer()
+    image = fields.Binary("Plant Image", attachment=True)
 
     @api.depends('order_ids')
     def _compute_order_count(self):
         for plant in self:
             plant.order_count = len(plant.order_ids)
+
+    @api.constrains('order_count', 'number_in_stock')
+    def _check_available_in_stock(self):
+        for plant in self:
+            if plant.number_in_stock and \
+                    plant.order_count > plant.number_in_stock:
+                raise UserError("There is only %s %s in stock but %s were sold"
+                                % (plant.number_in_stock, plant.name, plant.order_count))
